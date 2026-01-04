c86d69ef54c4dd047efe37638377ce5982c920c4
SebassGhost <sq07369@gmail.com>
2026-01-04 16:46:16 -0500

    Update run.sh

--- launcher/run.sh
+++ launcher/run.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Uso: ./run.sh example.com"
+  exit 1
+fi
+
+python3 - << EOF
+from recon.domain_enum.domain_enum import run
+result = run("$1")
+print(result)
+EOF
 
