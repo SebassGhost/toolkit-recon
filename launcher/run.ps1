0f1bc4dc445e6877fa16ed569b07286052238485
SebassGhost <sq07369@gmail.com>
2026-01-04 16:37:13 -0500

    Update run.ps1

--- launcher/run.ps1
+++ launcher/run.ps1
@@ -1,2 +1,22 @@
-Write-Host ">>> probando python"
-python --version
+param (
+    [string]$Target
+)
+
+Write-Host ">>> run.ps1 iniciado"
+Write-Host ">>> Target:" $Target
+
+$ProjectRoot = Split-Path -Parent $PSScriptRoot
+Write-Host ">>> Project root:" $ProjectRoot
+
+$pythonCode = @"
+import sys
+sys.path.insert(0, r"$ProjectRoot")
+
+from recon.domain_enum.domain_enum import run
+import json
+
+result = run("$Target")
+print(json.dumps(result, indent=2))
+"@
+
+python -c $pythonCode
