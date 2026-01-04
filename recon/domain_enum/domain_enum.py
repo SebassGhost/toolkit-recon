a67b21e1233fa47fc6d9eb25149ce123e735d2f1
SebassGhost <sq07369@gmail.com>
2026-01-04 16:21:29 -0500

    Update domain_enum.py

--- recon/domain_enum/domain_enum.py
+++ recon/domain_enum/domain_enum.py
@@ -1,7 +1,22 @@
-def run(target):
+import socket
+
+def run(target: str) -> dict:
+    results = []
+
+    try:
+        ip = socket.gethostbyname(target)
+        results.append({
+            "domain": target,
+            "ip": ip
+        })
+    except Exception as error:
+        results.append({
+            "domain": target,
+            "error": str(error)
+        })
+
     return {
         "module": "domain_enum",
         "target": target,
-        "results": []
+        "results": results
     }
-
