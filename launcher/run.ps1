156d0ceaa8d9f5012a6d9a86aa6fd81bb9a31dc2
SebassGhost <sq07369@gmail.com>
2026-01-04 16:36:21 -0500

    Update run.ps1

--- launcher/run.ps1
+++ launcher/run.ps1
@@ -1,21 +1,2 @@
-Write-Host ">>> run.ps1 iniciado"
-
-param (
-    [string]$Target
-)
-
-if (-not $Target) {
-    Write-Host "Usage: .\run.ps1 <target>"
-    exit 1
-}
-
-$pythonCode = @"
-from recon.domain_enum.domain_enum import run
-import json
-
-result = run("$Target")
-print(json.dumps(result, indent=2))
-"@
-
-python -c $pythonCode
-
+Write-Host ">>> probando python"
+python --version
