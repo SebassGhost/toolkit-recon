ce499c7b23dd8c08c6c03cbc2d03051d293dfb7d
SebassGhost <sq07369@gmail.com>
2026-01-05 16:18:52 -0500

    Update recon_all.py

--- recon/recon_all.py
+++ recon/recon_all.py
@@ -3,6 +3,7 @@ import os
 
 from recon.domain_enum.domain_enum import run as domain_enum_run
 from recon.subdomain_enum.sub_enum import run as subdomain_enum_run
+from recon.endpoint_discovery.endpoints import run as endpoint_discovery_run
 # más módulos se agregarán aquí
 
 def run_all(target):
