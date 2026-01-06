4cecb2ec6eabff9a9e05253d53ca3d44b8dd0cfa
SebassGhost <sq07369@gmail.com>
2026-01-06 17:43:54 -0500

    Update fingerprint.py

--- recon/tech_fingerprint/fingerprint.py
+++ recon/tech_fingerprint/fingerprint.py
@@ -1,6 +1,31 @@
-def run(target):
-    return {
-        "module": "subdomain_enum",
+import requests
+import json
+from .detectors import (
+    detect_headers,
+    detect_server,
+    detect_frameworks
+)
+
+def run(target: str) -> dict:
+    if not target.startswith("http"):
+        url = f"https://{target}"
+    else:
+        url = target
+
+    results = {
+        "module": "tech_fingerprint",
         "target": target,
-        "results": []
+        "results": {}
     }
+
+    try:
+        response = requests.get(url, timeout=8, allow_redirects=True)
+    except requests.RequestException as e:
+        results["error"] = str(e)
+        return results
+
+    results["results"]["server"] = detect_server(response)
+    results["results"]["headers"] = detect_headers(response)
+    results["results"]["frameworks"] = detect_frameworks(response)
+
+    return results
